9c2a0e924420128e73f93a59367cc2abe1bc1692
Srujal Magneto <srujal.magneto@gmail.com>
2019-09-21 12:35:05 +0530

    Implement two methods like arrayRefactorPop and arrayRefactorPush arrayRefactorPop method will delete key provided in second param and arrayRefactorPush method will not delete key provided in second param

--- array-refactor.js
+++ array-refactor.js
@@ -7,37 +7,96 @@
 
 'use strict';
 
-// Main function for array refactor
-let hideSensitiveData = (arr, arrayOfKeys) => {
+// Remove key from object function for array refactor
+let arrayRefactorPop = (arr, arrayOfKeys) => {
   let temp = [];
   // check given varibale is type of array or object
-  if (arr && typeof arr === 'object') {
+  if (arr && typeof arr === 'object' && arrayOfKeys) {
     if (Array.isArray(arr)) {
       temp = arr.map(d => {
-        return deleteKey(d);
+        return deleteKey(d, arrayOfKeys);
       });
-      // This recursion function is heart of array-refactor
-      function deleteKey(insideArr) {
-        if (Array.isArray(insideArr)) {
-          return insideArr.map(d => {
-            for (let prop in d) {
-              if (typeof d[prop] === 'object' || Array.isArray(d[prop]))
-                deleteKey(d[prop]);
-            }
-            removeKey(d, arrayOfKeys);
-            return d;
-          })
-        } else {
-          removeKey(insideArr, arrayOfKeys)
-          return insideArr;
-        }
-      }
     } else {
+      // console.log()
+      checkProperty(arr, arrayOfKeys);
       removeKey(arr, arrayOfKeys)
       temp = arr;
     }
   }
-  return temp;
+  if ((arr && typeof arr === 'object') || temp.length > 0 || Object.keys(temp).length > 0) {
+    return temp;
+  } else {
+    if (!arrayOfKeys) {
+      throw new Error("Second Parameter Should Be String Or Array Of Strings");
+    } else {
+      throw new Error("First Parameter Should Be Object Or Array Of Objects");
+    }
+  }
+}
+
+// Push object from object function for array refactor
+let arrayRefactorPush = (arr, arrayOfKeys) => {
+  let temp = [];
+  // check given varibale is type of array or object
+  if (arr && typeof arr === 'object' && arrayOfKeys) {
+    if (Array.isArray(arr)) {
+      temp = arr.map(d => {
+        let obj = {};
+        if (Array.isArray(arrayOfKeys)) {
+          arrayOfKeys.forEach(k => {
+            if (d.hasOwnProperty(k)) {
+              // console.log(obj[k]);
+              obj[k] = d[k];
+            }
+          });
+        } else {
+          if (d.hasOwnProperty(arrayOfKeys)) {
+            obj[arrayOfKeys] = d[arrayOfKeys];
+          }
+        }
+        return obj;
+      })
+    } else {
+      let obj = {};
+      if (Array.isArray(arrayOfKeys)) {
+        arrayOfKeys.forEach(k => {
+          if (arr.hasOwnProperty(k)) {
+            obj[k] = arr[k];
+          }
+        });
+      } else {
+        if (arr.hasOwnProperty(arrayOfKeys)) {
+          obj[arrayOfKeys] = arr[arrayOfKeys];
+        }
+      }
+      temp = obj;
+      // return temp;
+    }
+  }
+  if ((arr && typeof arr === 'object') || temp.length > 0 || Object.keys(temp).length > 0) {
+    return temp;
+  } else {
+    if (!arrayOfKeys) {
+      throw new Error("Second Parameter Should Be String Or Array Of Strings");
+    } else {
+      throw new Error("First Parameter Should Be Object Or Array Of Objects");
+    }
+  }
+}
+
+// This recursion function is heart of array-refactor
+function deleteKey(insideArr, arrayOfKeys) {
+  if (Array.isArray(insideArr)) {
+    return insideArr.map(d => {
+      checkProperty(d, arrayOfKeys);
+      removeKey(d, arrayOfKeys);
+      return d;
+    })
+  } else {
+    checkProperty(insideArr, arrayOfKeys);
+    removeKey(insideArr, arrayOfKeys)
+    return insideArr;
+  }
 }
 
 // remove key from object
@@ -55,4 +114,17 @@ function removeKey(insideArr, arrayOfKeys) {
   }
 }
 
-module.exports = hideSensitiveData;
+// find out property is having an array or object
+function checkProperty(arrObj, arrayOfKeys) {
+  for (let prop in arrObj) {
+    if (typeof arrObj[prop] === 'object' || Array.isArray(arrObj[prop])) {
+      deleteKey(arrObj[prop], arrayOfKeys);
+    }
+  }
+}
+
+module.exports = {
+  arrayRefactorPop,
+  arrayRefactorPush
+
+};
